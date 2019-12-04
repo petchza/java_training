09dd1d15f8e5975a862899153d8fbfed06dbd50a
PopatY <maxbaraja_max@hotmail.com>
2019-12-04 15:47:29 +0700

    1st : change do-while to while, update condition

--- TestJava/src/code/test_0000.java
+++ TestJava/src/code/test_0000.java
@@ -5,29 +5,30 @@ import java.util.Scanner;
 public class test_0000 {
 
 	public static void main(String[] args) {
-		
+
 		String a = "";
 		String b = "";
 		Scanner scanner =  null;
-		do { 
+		//1st : change do-while to while, update condition
+		do {
 			try {
 				scanner = new Scanner(System.in);
-				
+
 				System.out.print("Enter a : ");
 				a  = scanner.nextLine();
-				
+
 				if(a.equalsIgnoreCase("q")) {
 					break;
 				}
-				
+
 				System.out.print("Enter b : ");
 				 b  = scanner.nextLine();
-				
+
 				 if(b.equalsIgnoreCase("q")) {
 						break;
 					}
-				 
-				if(Integer.valueOf(a) >= 0 && Integer.valueOf(a) <= Math.pow(10, 9) && Integer.valueOf(b) >= 0 && Integer.valueOf(b) <= Math.pow(10, 9)) {           
+
+				if(Integer.valueOf(a) >= 0 && Integer.valueOf(a) <= Math.pow(10, 9) && Integer.valueOf(b) >= 0 && Integer.valueOf(b) <= Math.pow(10, 9)) {
 					System.out.println("a + b = "+ (Integer.valueOf(a)+Integer.valueOf(b))+"\n");
 				}else {
 					System.out.println("Error value.");
@@ -35,10 +36,10 @@ public class test_0000 {
 			}catch(NumberFormatException e) {
 				System.out.println("error type");
 			}
-		
-		}while(!a.equalsIgnoreCase("q") || !b.equalsIgnoreCase("q")); 
+
+		}while(!a.equalsIgnoreCase("q") || !b.equalsIgnoreCase("q"));
 			System.out.println("end");
 			scanner.close();
-		
+
 	}
 }
