efe81e566a88af69b9a6e51daca003ab992844c4
PopatY <maxbaraja_max@hotmail.com>
2019-12-11 19:40:19 +0700

    add comment

--- TestJava/src/code/test_0003.java
+++ TestJava/src/code/test_0003.java
@@ -6,7 +6,7 @@ public class test_0003 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		
+
 		String q = "";
 		while(!q.equalsIgnoreCase("y")) {
 		System.out.print("Enter matrix m : ");
@@ -15,6 +15,9 @@ public class test_0003 {
 		System.out.print("Enter matrix n : ");
 		int n = scanner.nextInt();
 		System.out.println();
+
+		//MAX: USING PARENTHESIS FOR MULTIPLE IF CONDITION
+		//if( (m <= 100 && m >= 1) && (n <= 100 && n >= 1))
 		//check condition m and n is 1-100
 		if(m <= 100 && m >= 1 && n <= 100 && n >= 1) {
 			int[][] a = new int[m][n];
@@ -24,12 +27,12 @@ public class test_0003 {
 //					System.out.print("Enter martrix a : ");
 					a[i][j] = scanner.nextInt();
 				}
-	
+
 			}
 			System.out.println("--------------------------");
 			for (int g = 0; g < m; g++) {
 				for (int h = 0; h < n; h++) {
-//					System.out.print("Enter martrix b : ");
+//					System.out.print("Enter matrix b : ");
 					b[g][h] = scanner.nextInt();
 				}
 			}
@@ -41,7 +44,7 @@ public class test_0003 {
 		scanner.close();
 		System.out.println("End");
 	}
-	
+
 	private static void result(int m, int n, int[][] a, int[][] b) {
 		int[][] c= new int[m][n];
 		System.out.println("--------------------------");
@@ -53,5 +56,5 @@ public class test_0003 {
 			System.out.println();
 		}
 	}
-	
+
 }
