ea0bf8a105b8992aa8d564bf8ecb18ddd2aa0b43
PopatY <maxbaraja_max@hotmail.com>
2019-12-16 12:38:10 +0700

    more quest

--- TestJava/src/code/test_0005.java
+++ TestJava/src/code/test_0005.java
@@ -6,13 +6,20 @@ import java.util.Scanner;
 public class test_0005 {
 	public static void main(String[] args) {
 
+		//Interfere Mission
+		//Add more input for specific which side that want to find (a ,b or c)
+		//Example
+		//Enter triangle side : (value a, b or c)
+		//calculate to find other side
+
+
 		Scanner scanner = new Scanner(System.in);
 		String q = "";
 		while(!q.equalsIgnoreCase("y")) {
 			//Step 1 get input value of type double
 			System.out.print("Enter a : ");
 			double a = scanner.nextDouble();
-			
+
 			System.out.print("Enter b : ");
 			double b = scanner.nextDouble();
 			//Step 2 pass value both to method calculate pythagoras
@@ -20,7 +27,7 @@ public class test_0005 {
 			//Step 3 set DecimalFormat for set 6 decimal points and show the result
 			DecimalFormat decimalFormat = new DecimalFormat("#.000000");
 			System.out.println(decimalFormat.format(result));
-			
+
 			System.out.println("Do you want to exit?");
 			q = scanner.next();
 		}
