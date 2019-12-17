b342caf0ff47fe22b8cad3e6f792747944db4edb
petchza <anon.phaoinjun@gmail.com>
2019-12-17 10:16:35 +0700

    Edited 0004

--- TestJava/src/code/test_0004.java
+++ TestJava/src/code/test_0004.java
@@ -2,7 +2,6 @@ package code;
 
 import java.util.Scanner;
 
-
 public class test_0004 {
 	public static void main(String[] args) {
 
@@ -11,13 +10,17 @@ public class test_0004 {
 		String q = "";
 		while(!q.equalsIgnoreCase("y")) {
 			try {
-			//Step1 get input String
+
 			System.out.print("Enter string : ");
 			String input = scanner.next();
-			//Step2 check condition and pass string to method
+
 			if(input.length() <= 10000) {
 				System.out.println(checkCharacterUpperLowerAndMix(input));
 				System.out.println("----------------------");
+			}else {
+				System.out.println("Error length String!");
+				System.out.println("----------------------");
+				System.out.println();
 			}
 			System.out.println("Do you want to exit?");
 			q = scanner.next();
@@ -34,28 +37,40 @@ public class test_0004 {
 	
 	private static String checkCharacterUpperLowerAndMix(String value) {
 		
-		//Step 3 convert string to array char for check Uppercase Lowercase and Mix one by one
-		char[] inputChar = value.toCharArray();
-		boolean statusUpper = false;
-		boolean statusLower = false;
 		String result = "";
-		//Step 4 check Uppercase Lowercase and Mix in array char
-		for(int i = 0 ;i<value.length(); i++) {
-			if(Character.isUpperCase(inputChar[i])) {
-				statusUpper = true;
-			}else{
-				statusLower = true;
-			}
-		}
-		
-		//Step 5 check status Uppercase Lowercase and Mix for set string and return 
-		if(statusUpper == true && statusLower == true) {
-			result = "Mix";
-		}else if(statusUpper == true && statusLower == false) {
-			result = "All Capital Letter";
-		}else {
-			result = "All Small Letter";
-		}
+		boolean statusUpper =  isUpperCase(value); 
+		boolean statusLower =  isLowerCase(value); 
+			
+			if(statusUpper && !statusLower) {
+				result = "All Capital Letter";
+			}else {
+				if(statusLower) {
+					result = "All Small Letter";
+				}else {
+					result = "Mix";
+				}
+				
+			}		
 		return result;
 	}	
+	
+	private static boolean isUpperCase(String value)
+	{
+	    for(char c : value.toCharArray())
+	    {
+	        if(!Character.isUpperCase(c))
+	            return false;
+	    }
+	    return true;
+	}
+	
+	private static boolean isLowerCase(String value)
+	{
+	    for(char c : value.toCharArray())
+	    {
+	        if(!Character.isLowerCase(c))
+	            return false;
+	    }
+	    return true;
+	}
 }
