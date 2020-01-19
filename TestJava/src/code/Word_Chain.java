f05dbd53aaddf0a91289ce6c0bcfcbef84e80c6f
petchza <anon.phaoinjun@gmail.com>
2020-01-19 21:11:22 +0700

    Edit word chain

--- TestJava/src/code/Word_Chain.java
+++ TestJava/src/code/Word_Chain.java
@@ -3,27 +3,28 @@ package code;
 import java.util.Random;
 import java.util.Scanner;
 
+import com.sun.org.apache.xml.internal.utils.StringComparable;
+
 public class Word_Chain {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String q = "";
-		while (!q.equalsIgnoreCase("y")) {
+		String quit = "";
+		while (!quit.equalsIgnoreCase("y")) {
 			try {
 
-				System.out.print("Enter L : ");
-				int l = scanner.nextInt();
+				System.out.print("Enter length : ");
+				int lengthOfString = scanner.nextInt();
 	
-				System.out.print("Enter N : ");
-				int n = scanner.nextInt();
-	
-				if ((l >= 3) && (l <= 1000) && (n >= 1) && (n <= 30000)) {
-					genString(l,n);
-				} else {
-					System.out.println("Error length of l or n");
+				System.out.print("Enter number : ");
+				int numberOfString = scanner.nextInt();
+				
+				if(validateInput(lengthOfString,numberOfString)) {
+					genString(lengthOfString,numberOfString);
+				}else {
+					System.out.println("Error length 3-1000 or number 1-3000");
 				}
-	
 				System.out.print("Do you want to exit? (y/n) : ");
-				q = scanner.next();
+				quit = scanner.next();
 				
 			}catch(NumberFormatException e) {
 				System.out.println("Error type.");
@@ -33,14 +34,24 @@ public class Word_Chain {
 		scanner.close();
 	}
 	
-	private static void genString(int l, int n) {
+	private static boolean validateInput(int lengthOfString, int numberOfString) {
+		boolean status = false;
+		if ((lengthOfString >= 3) && (lengthOfString <= 1000)) {
+			if((numberOfString >= 1) && (numberOfString <= 30000)) {
+				status =  true;
+			}
+		} 
+		return status;
+	}
+	
+	private static String genString(int length, int number) {
 		String pattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		Random random = new Random();
 		String valueB = "";
-		for(int i = 0;i<n;i++) {		
+		for(int i = 0;i<number;i++) {		
 			char c;
 			StringBuilder valueA = new StringBuilder();
-			for(int j = 0;j<l;j++) {
+			for(int j = 0;j<length;j++) {
 				if(i == 0 || random.nextInt(pattern.length()) % 2 ==0) {
 					c = pattern.charAt(random.nextInt(pattern.length()));
 				}else {
@@ -49,19 +60,20 @@ public class Word_Chain {
 				valueA.append(c);
 			}
 			System.out.println(valueA.toString());
-			String output = checkWordChain(valueA,valueB,l);
+			String output = checkWordChain(valueA,valueB,length);
 			if(output != "") {
 				System.out.println(output);
 				break;
 			}
 			valueB = valueA.toString();
-		}			
+		}
+		return valueB;			
 	}
 
 	private static String checkWordChain(StringBuilder valueA, String valueB,int l) {
 		String output = "";
 		int count = 0;
-		if(valueB != "") {
+		if(valueB != "") {	
 			for(int i=0;i<l;i++) {
 				if(valueA.charAt(i) != valueB.charAt(i)) {
 					count++;
