f8023dc993c75bc67584c5dc12cc5033d9008cd9
petchza <anon.phaoinjun@gmail.com>
2020-01-26 19:54:04 +0700

    Dice pending

--- TestJava/src/code/Dice.java
+++ TestJava/src/code/Dice.java
@@ -9,6 +9,7 @@ public class Dice {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String quit = "";
+		boolean statusStringAction;
 		while (!quit.equalsIgnoreCase("y")) {
 			try {
 				List<String> action = new ArrayList<>();
@@ -18,9 +19,10 @@ public class Dice {
 				
 				if((number >=1) && (number <=6)) {
 					action = getStringAction(scanner,number);
-					if(validateInput(action,number)) {
-//						diceAction();
-						System.out.println("Work");
+					statusStringAction = validateStringAction(action,number);
+					if(statusStringAction) {
+						System.out.println(diceAction(action,number));  
+//						diceAction(action,number);
 					}else {
 						System.out.println("Error string action not 1-1000 or value not match 'B,C,D,F,L,R'");
 					}
@@ -28,10 +30,8 @@ public class Dice {
 					System.out.println("Error length number. (1-6)");
 					System.out.println("");
 				}
-
 			}catch(InputMismatchException e) {
 				System.out.println("Error type number.");
-		
 			}
 			System.out.println("Do you want to exit? (y/n) : ");
 			quit = scanner.next();
@@ -40,12 +40,133 @@ public class Dice {
 		scanner.close();
 	}
 
-	private static void diceAction() {
-		
-		
+	private static String diceAction(List<String> action, int number) {
+		int result = 0;
+		for (int i = 0; i < number; i++) {
+			String actionString = action.get(i);
+			result = doAction(actionString);
+			System.out.println(result);
+		}
+		return "END";
 	}
 	
-	private static boolean validateInput(List<String> action, int number) {
+	private static int doAction(String actionString) {
+		int top = 1, font = 2, left = 3, back = 5, rigth = 4, bottom = 6;
+		int diceTop = 0, diceFont = 0, diceLeft = 0, diceBack = 0, diceRigth = 0, diceBottom = 0;
+		int tempTop = 0,tempFont = 0;
+		for(int count=0;count<actionString.length();count++) {
+			switch(actionString.charAt(count)) {
+			case 'F': {
+				if(count == 0) {
+					diceTop = back;
+					diceFont = top;
+					diceLeft = left;
+					diceBack = bottom;
+					diceRigth = rigth;
+					diceBottom = font;
+				}else {
+					tempTop = diceTop;
+					diceTop = diceBack;
+					diceBack = diceBottom;
+					diceBottom = diceFont;
+					diceFont = tempTop;
+				}
+				break;
+			}  
+			case 'B': {
+				if(count == 0) {
+					diceTop = font;    
+					diceFont = bottom;
+					diceLeft = left;
+					diceBack = top;
+					diceRigth = rigth;
+					diceBottom = back;
+				}else {
+					tempTop = diceTop;
+					diceTop = diceFont;
+					diceFont = diceBottom;
+					diceBottom = diceBack;
+					diceBack = diceTop;
+				} 
+				break;
+			}
+			case 'L':  {
+				if(count == 0) {
+					diceTop = rigth;    
+					diceFont = font;
+					diceLeft = top;
+					diceBack = back;
+					diceRigth = bottom;
+					diceBottom = left;
+				}else {
+					tempTop = diceTop;
+					diceTop = diceRigth;
+					diceRigth = diceBottom;
+					diceBottom = diceLeft;
+					diceLeft = diceTop;
+				} 
+				break;
+			}
+			case 'R':  {
+				if(count == 0) {
+					diceTop = left;    
+					diceFont = font;
+					diceLeft = bottom;
+					diceBack = back;
+					diceRigth = top;
+					diceBottom = rigth;
+				}else {
+					tempTop = diceTop;
+					diceTop = diceLeft;
+					diceLeft = diceBottom;
+					diceBottom = diceRigth;
+					diceRigth = tempTop;
+				} 
+				break;
+			}
+			
+			case 'C': {
+				if(count == 0) {
+					diceTop = top;    
+					diceFont = rigth;
+					diceLeft = font;
+					diceBack = left;
+					diceRigth = back;
+					diceBottom = bottom;
+				}else {
+					tempFont = diceFont;
+					diceFont = diceRigth;
+					diceRigth = diceBack;
+					diceBack = diceLeft;
+					diceLeft = tempFont;
+				} 
+				break;
+			}  
+			
+			case 'D': {
+				if(count == 0) {
+					diceTop = top;    
+					diceFont = left;
+					diceLeft = back;
+					diceBack = rigth;
+					diceRigth = font;
+					diceBottom = bottom;
+				}else {
+					tempFont = diceFont;
+					diceFont = diceLeft;
+					diceLeft = diceBack;
+					diceBack = diceRigth;
+					diceRigth = tempFont;
+				} 
+				break;
+			   } 
+			}
+		}
+		
+		return diceFont;
+	}
+
+	private static boolean validateStringAction(List<String> action, int number) {
 		String pattern = "BCDFLR";
 		boolean ststus = true;
 		for(int i = 0;i<number;i++) {
@@ -53,10 +174,19 @@ public class Dice {
 				ststus = false;
 				break;
 			}else {
-//				if(pattern.contains(action.get(i))){
-//					ststus = false;
-//					break;
-//				}
+				ststus = checkPattern(action,i,pattern,ststus);
+			}
+		}
+		return ststus;
+	}
+
+	private static boolean checkPattern(List<String> action, int i, String pattern, boolean ststus) {
+		String singleWord = action.get(i);
+		for(int count=0;count<singleWord.length();count++) {
+			int find = pattern.indexOf(singleWord.charAt(count));
+			if(find == -1) {
+				ststus = false;
+				break;
 			}
 		}
 		return ststus;
